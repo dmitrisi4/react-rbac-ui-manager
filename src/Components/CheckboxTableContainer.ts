f197b197a1e196f8ce2e093924130573c9d6cc49
Dimitri <73295617+dmitrisi4@users.noreply.github.com>
2023-12-05 14:05:34 +0100

    Update CheckboxTableContainer.ts

--- src/Components/CheckboxTableContainer.ts
+++ src/Components/CheckboxTableContainer.ts
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import TableContainer from "@material-ui/core/TableContainer";
 
 const CheckboxTableContainer = styled(TableContainer)`
-    background: rgba(214, 235, 255, 0.4);
+    background: rgb(249 255 250);
+    box-shadow: 1px 1px 12px 5px #e7e7e7;
+    border-left: 0.5px solid rgb(2 167 112 / 50%);
 `;
 
 export default CheckboxTableContainer;
