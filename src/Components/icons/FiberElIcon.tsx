0d82964d964744370309bd39f783c218f81116b9
Dimitri <73295617+dmitrisi4@users.noreply.github.com>
2023-12-05 14:04:37 +0100

    added icons

---
+++ src/Components/icons/FiberElIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+const FiberElIcon = () => {
+	return (<svg width="21px" height="21px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" 
+		fill="#000000">
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+		<g id="SVGRepo_iconCarrier">
+		<rect x="2.25" y="11.03" width="19.5" height="1.95" fill="#76c59d"></rect>
+		<circle cx="12" cy="12" r="6.82" fill="#4db686"></circle>
+		<path d="M14.44,10.05V9.56a2.44,2.44,0,1,0-4.88,0v.49a1,1,0,0,0-1,1v3.89a1,1,0,0,0,1,1h4.88a1,1,0,0,0,1-1V11A1,1,0,0,0,14.44,10.05Z" 
+			fill="#fff"></path>
+			<path d="M13.66,10.05H10.34V9.56a1.66,1.66,0,0,1,3.32,0Z" 
+				fill="#4db686"></path>
+				<circle cx="12" cy="12.97" r="0.97" fill="#00a76f">
+				</circle></g>
+		</svg>);
+};
+
+export default FiberElIcon;
---
+++ src/Components/icons/MenuIcon.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const MenuIcon = () => {
+	return (<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" 
+	xmlns="http://www.w3.org/2000/svg">
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+		<g id="SVGRepo_iconCarrier"> 
+			<g id="Menu / Menu_Alt_04"> 
+				<path id="Vector" d="M5 17H19M5 12H19M5 7H13" 
+				stroke="#00A76F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> 
+			</g> 
+		</g></svg>);
+};
+
+export default MenuIcon;
