b38641b83f73ec067d5ce9352a8f28d9a4d29b82
Dimitri <73295617+dmitrisi4@users.noreply.github.com>
2023-12-05 14:05:20 +0100

    Update RoleTag.ts

--- src/Components/RoleTag.ts
+++ src/Components/RoleTag.ts
@@ -5,8 +5,11 @@ const RoleTag = withTheme(styled.span`
     text-transform: uppercase;
     color: ${(props: any) => props.theme.palette.primary.light};
     background: ${(props: any) => props.theme.palette.info.main};
-    padding: 4px;
+    padding: 4px 6px;
     white-space: nowrap;
+    border-radius: 8px;
+    font-size: 13px;
+    font-weight: 700;
 `);
 
 export default RoleTag;
